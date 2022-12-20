4a0b98bc8a4499c6c0e6d2d74bc85ab8bd5fe777
lin <Cython_lin@cklin.top>
2022-12-20 22:13:49 +0800

    delete echo path
    delete echo path

--- bin/j.bat
+++ bin/j.bat
@@ -5,7 +5,6 @@ echo %*|>nul findstr /rx \-.*
 if ERRORLEVEL 1 (
   for /f "delims=" %%i in ('python "%~dp0\autojump" %*') do set new_path=%%i
   if exist !new_path!\nul (
-    echo !new_path!
     pushd !new_path!
     REM endlocal is necessary so that we can change directory for outside of this script
     REM but will automatically popd. We mush pushd twice to work around this.
